07868733a6875046ed77ba44ccfdb39902d7775e
camilaDiMontezemolo <camiladimon@hotmail.com>
2022-09-11 21:53:30 -0300

    Api bajada, se ve la foto y el nombre del personaje resultado

--- src/CharacterInfoPage.js
+++ src/CharacterInfoPage.js
@@ -2,10 +2,32 @@ import React from 'react';
 import {View, Text, Image, TextInput, Button} from 'react-native';
 
 const CharacterInfoPage = () => {
+    const [characterInfo, setCharacterInfo] = React.useState('')
+    const [loading, setLoading] = React.useState(false)
+    const [url, setUrl] = React.useState('https://rickandmortyapi.com/api/character/578')
+    
+    React.useEffect(() => { 
+        getCharacter(); 
+    }, [])  // los primeros parentesis no hacen nada, donde van las llaves va el código, los corchetes tienen las variables de estado? Funciona como componentDidMount
+    //como en la llave va lo que uso en use efect, pongo el getCharacter ahí
+    //Si pongo los corchetes afuera del parentesis, me refreshea al instante!!
+    function getCharacter(){
+        setLoading(true)
+    
+        fetch (url)
+          .then (res => res.json()) /** una vez que el servidor responde, la respuesta se convierte en json */
+          .then( res => {
+            console.log(res)
+            setCharacterInfo(res)
+            setLoading(false)
+          });
+        };
+
     return (
         <View>
-            <Text>Character Name</Text>
-            <Image source={require('../img/rick_and_morty_logo.png')}/>
+            <Text>Character Name: { characterInfo.name }</Text>
+            <Image style={{ height: 180, width: 180}}
+            source={{uri: characterInfo.image}}/>
 
             <Text>Information</Text>
             <Text>Status</Text><Text>Character Status</Text>
